549e4a3182b05563f785a0683e22a758ff953c6d
TadeoAguirre <Tadeoaguirre32@gmail.com>
2024-09-22 22:40:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("hola mundo");
+    printf("\n hola mundo");
+    printf("\n X  \n Y");
+    printf("\n\n hola mundo \a");
+    printf("\n\n\n  \hola mundo");
+}
